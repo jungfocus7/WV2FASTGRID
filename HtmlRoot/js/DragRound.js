3ce5f6303c9e9edee175f9462d36d2cd87ac004b
JUNGFOCUS7 <jungfocus7@gmail.com>
2023-06-01 05:38:53 +0900

    ``

--- HtmlRoot/js/DragRound.js
+++ HtmlRoot/js/DragRound.js
@@ -7,48 +7,40 @@ const _MouseDown = 'mousedown';
 class DragRound {
     constructor(dnm) {
         this.#m_dgrid = document.getElementById(dnm);
-
-        this.#m_dx = 0;
-        this.#m_dy = 0;
-
-        this.#m_sl = 0;
-        this.#m_st = 0;
-
         window.addEventListener(_MouseDown, this.#fn_MouseDown);
         Object.freeze(this);
     }
 
     #m_dgrid = null;
-
+    #m_bmd = false;
     #m_dx = 0;
     #m_dy = 0;
-
     #m_sl = 0;
     #m_st = 0;
 
 
     #fn_MouseMove = (e) => {
-        // console.log(_MouseMove);
-        // console.log(e);
+        // console.log(_MouseMove, e);
+        if (this.#m_bmd === false) return;
 
         let lx = this.#m_dx - e.x;
         let ly = this.#m_dy - e.y;
         // console.log(lx, ly);
-
         let sl = this.#m_sl + lx;
         let st = this.#m_st + ly;
         // console.log(sl, st);
-
         this.#m_dgrid.scroll(sl, st);
     };
 
 
     #fn_MouseUp = (e) => {
-        // console.log(_MouseUp);
+        console.log(_MouseUp, e);
+        if (this.#m_bmd === false) return;
 
         window.removeEventListener(_MouseMove, this.#fn_MouseMove);
         window.removeEventListener(_MouseUp, this.#fn_MouseUp);
         window.removeEventListener(_Blur, this.#fn_MouseUp);
+        this.#m_bmd = false;
 
         // let sl = this.#m_dgrid.scrollLeft;
         // let st = this.#m_dgrid.scrollTop;
@@ -57,20 +49,21 @@ class DragRound {
 
 
     #fn_MouseDown = (e) => {
-        // console.log(_MouseDown);
-
+        // console.log(_MouseDown, e);
         if (e.button === 0) {
+            if (this.#m_bmd === true) return;
+
             window.addEventListener(_MouseMove, this.#fn_MouseMove);
             window.addEventListener(_MouseUp, this.#fn_MouseUp);
             window.addEventListener(_Blur, this.#fn_MouseUp);
+            this.#m_bmd = true;
 
             this.#m_dx = e.x;
             this.#m_dy = e.y;
             // console.log(this.#m_dx, this.#m_dy);
-            // console.log(this);
-
             this.#m_sl = this.#m_dgrid.scrollLeft;
             this.#m_st = this.#m_dgrid.scrollTop;
+            // console.log(this.#m_sl, this.#m_st);
 
             this.#fn_MouseMove(e);
         }
